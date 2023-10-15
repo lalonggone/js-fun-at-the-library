588897cd39de32e4de62afe5d85e998ca0b6fe34
Laura Long <lauralonggone@gmail.com>
2023-10-14 21:09:23 -0600

    Finish book-test

--- src/book.js
+++ src/book.js
@@ -1,12 +1,47 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return "The " + title;
 }
+
+function buildMainCharacter(charName, charAge, charPronouns) {
+  return {
+    name: charName,
+    age:charAge,
+    pronouns: charPronouns,
+  }
+}
+
+function saveReview(quote, reviews) {
+  if (!reviews.includes(quote)) {
+    reviews.push(quote);
+  }
+}
+
+function calculatePageCount(bookTitle) {
+  return bookTitle.length * 20;
+}
+
+function writeBook(bookTitle, bookCharacter, genre) {
+
+var pageCount = bookTitle.length * 20;
+
+  return {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    genre: genre,
+    pageCount: pageCount
+  }
+}
+
+function editBook(book) {
+  book.pageCount = book.pageCount * 3/4;
+}
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
