5dc35c0096c4e002fb03b217a11a529409e7f309
Laura Long <lauralonggone@gmail.com>
2023-10-15 16:34:08 -0600

    Finish shelf-test

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,42 @@
+function shelfBook(book, shelf) {
+if (shelf.length < 3) {
+  shelf.unshift(book);
+}
+}
+
+function unshelfBook(bookName, shelf) {
+  var index = shelf.findIndex(book => book.title === bookName);
+
+  if (index !== -1) {
+    shelf.splice(index, 1);
+  }
+}
+
+function unshelfBook(bookName, shelf) {
+  for (let i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === bookName) {
+      shelf.splice(i, 1);
+    }
+  }
+}
+
+function listTitles(shelf) {
+  var titles = [];
+  for (var i = 0; i < shelf.length; i++) {
+    titles.push(shelf[i].title);
+  }
+  return titles.join(', ');
+}
+
+function searchShelf(shelf, book) {
+  
+
+}
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
