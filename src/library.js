c6a1b885702360278d380f7180908b04c61cd5bd
Laura Long <lauralonggone@gmail.com>
2023-10-15 23:55:38 -0600

    Finish half of library.js and remove skips in book-tests.js and allow me to sleep

--- src/library.js
+++ src/library.js
@@ -1,8 +1,27 @@
+function createLibrary(libraryName) {
+  var library = {
+    name: libraryName,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: [],
+    }
+  }
+  return library
+}
 
+function addBook(library, book) {
+  if (book.genre === 'fantasy'){
+    library.shelves.fantasy.push(book)}
+  else if (book.genre === 'fiction'){
+    library.shelves.fiction.push(book)}
+  else if (book.genre === 'nonFiction'){
+    library.shelves.nonFiction.push(book)}
+}
 
 module.exports = {
-  // createLibrary,
-  // addBook,
+  createLibrary,
+  addBook,
   // checkoutBook,
   // takeStock
 };
